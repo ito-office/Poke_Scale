b78ca78705e88dd65ce03851edbb37b17ac25b9e
ito-office <ito.office.it@gmail.com>
2026-07-06 16:30:54 +0900

    main.jsx更新

--- pokemon-number-game/src/main.jsx
+++ pokemon-number-game/src/main.jsx
@@ -3,4 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-createRoot(document.getElementById('root')).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
